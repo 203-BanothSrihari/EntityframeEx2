a78c54ef32b8d32472b8e95efecfe01e1143cc70
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 17:11:45 +0530

    Add project files.

---
+++ EntityframeEx2/Data/EntityframeEx2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityframeEx2.Models;
+
+namespace EntityframeEx2.Data
+{
+    public class EntityframeEx2Context : DbContext
+    {
+        public EntityframeEx2Context (DbContextOptions<EntityframeEx2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityframeEx2.Models.Product> Product { get; set; }
+    }
+}
---
+++ EntityframeEx2/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityframeEx2.Models
+{
+    public class Product
+    {
+        public int ID { set; get; }
+
+        public string Name { get; set; }
+
+        public string Shop { get; set; }
+
+        public int Cost { get; set; }
+    }
+
+}
